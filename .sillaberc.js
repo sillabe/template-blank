a0659538aa7271f5eb38fa18ab0b000451c26856
Alberto <alberteddu@users.noreply.github.com>
2021-05-30 11:58:42 +0200

    feat: first commit

---
+++ .sillaberc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: 'content',
+    build: 'build',
+    views: 'views',
+    templateEngine: 'nunjucks',
+    plugins: [
+        '@sillabe/private-nodes-plugin',
+        '@sillabe/filesystem-sorting-plugin',
+    ],
+};
